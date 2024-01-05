115dac78ba7b6ecd5b10974c594e9e1a0df47753
1MinutePlease <paulflohr08@gmail.com>
2024-01-05 16:37:33 +0100

    Revert "updated project to a Compose Desktop Application"
    This reverts commit 61715984202a72c24552ab366d357c8abe9e3070.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
